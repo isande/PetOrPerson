b81c0ef411669b625c8bf24ad83b4fdc5b4adba9
Peggy Wollenhaupt <isande@gmail.com>
2020-08-16 11:27:59 -0600

    initial commit

---
+++ PetOrPerson/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Peggy Wollenhaupt on 8/12/20.
+//  Copyright © 2020 Peggy Wollenhaupt. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("cannot convert to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
+            imageView.image = userPickedImage
+
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: PetImageClassifier().model) else {
+            fatalError("cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier.capitalized
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error: \(error)")
+        }
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# WhatFlower
+# PetOrPerson
